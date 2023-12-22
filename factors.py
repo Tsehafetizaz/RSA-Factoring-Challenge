24049646efdb4f40fe66e1bd7039c5902569b748
Tsehafetizaz <wendetefe@gmail.com>
2023-12-22 11:26:36 -0800

    piexe

--- factors.py
+++ factors.py
@@ -1,30 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
+import math
+import time
 
 
-def factorize(n):
-    for i in range(2, n):
-        if n % i == 0:
-            return i, n // i
-        return 1, n
+def find_factors(number):
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return i, number // i
+    return number, 1
 
 
-def main(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            numbers = file.readlines()
-            for number in numbers:
-                num = int(number.strip())
-                p, q = factorize(num)
-                print(f"{num}={p}*{q}")
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def factorize_file(filename):
+    with open(filename, 'r') as file:
+        for line in file:
+            number = int(line.strip())
+            start_time = time.time()
+            p, q = find_factors(number)
+            print(f"{number}={p}*{q}")
+            if time.time() - start_time > 5:
+                break
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: factors <file>")
-    else:
-        main(sys.argv[1])
+        sys.exit(1)
+
+    factorize_file(sys.argv[1])
